c29359428b5fd4be94aafb4786453025e8ed087f
Ijay Abby <inyangasi4@gmail.com>
2021-03-26 00:41:03 +0300

    Splice method to remove elements

---
+++ freecodecamp/splice_remove_method.js
@@ -0,0 +1,12 @@
+// splice() allows us to do just that: remove any number of consecutive elements from anywhere in an array.
+
+// splice() can take up to 3 parameters, but for now, we'll focus on just the first 2. The first two parameters of splice() are integers which represent indexes, or positions, of the array that splice() is being called upon. And remember, arrays are zero-indexed, so to indicate the first element of an array, we would use 0. splice()'s first parameter represents the index on the array from which to begin removing elements, while the second parameter indicates the number of elements to delete. 
+
+
+// We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4)
+// Only change code above this line
+console.log(arr);
