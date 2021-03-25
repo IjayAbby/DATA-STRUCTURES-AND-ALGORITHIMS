59ccfd89b5e9262e32d82f82d856cc153cb4cfef
Ijay Abby <inyangasi4@gmail.com>
2021-03-26 00:40:23 +0300

    Splice method to add

---
+++ freecodecamp/splice_add_method.js
@@ -0,0 +1,12 @@
+// Remember in the last challenge we mentioned that splice() can take up to three parameters? Well, you can use the third parameter, comprised of one or more element(s), to add to the array. This can be incredibly useful for quickly switching out an element, or a set of elements, for another.
+
+// We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
+
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
