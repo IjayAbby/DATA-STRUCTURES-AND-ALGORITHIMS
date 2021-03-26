2327bdc6e8d0d3010f1c7187771e694007458140
Ijay Abby <inyangasi4@gmail.com>
2021-03-26 17:21:29 +0300

    Complex multi-dimensional arrays

--- freecodecamp/multi-dimensional.js
+++ freecodecamp/multi-dimensional.js
@@ -1,1 +1,13 @@
 // One of the most powerful features when thinking of arrays as data structures, is that arrays can contain, or even be completely made up of other arrays. We have seen arrays that contain arrays in previous challenges, but fairly simple ones. However, arrays can contain an infinite depth of arrays that can contain other arrays, each with their own arbitrary levels of depth, and so on. In this way, an array can very quickly become very complex data structure, known as a multi-dimensional, or nested array.
+
+// We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string deep, on the fourth level, include the string deeper, and on the fifth level, include the string deepest.
+
+let myNestedArray = [
+    // Only change code below this line
+    'level 1',                   /* myNestedArray[0]             */
+    ['level 2'],	               /* myNestedArray[1][0]          */
+    [['level 3','deep']],	       /* myNestedArray[2][0][0]       */
+    [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
+    [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
+    // Only change code above this line
+  ];
