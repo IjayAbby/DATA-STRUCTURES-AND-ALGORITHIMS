e8454b92f5bb4e9b42343e9b1de9fe0c311cd0ed
Ijay Abby <inyangasi4@gmail.com>
2021-04-02 23:27:59 +0300

    Property of an object

--- freecodecamp Basic Data Structures/check_obj_property.js
+++ freecodecamp Basic Data Structures/check_obj_property.js
@@ -4,7 +4,6 @@
 
 users.hasOwnProperty('Alan');
 'Alan' in users;
-
 // Both of these would return true.
 
 // We've created an object, users, with some users in it and a function isEveryoneHere, which we pass the users object to as an argument. Finish writing this function so that it returns true only if the users object contains all four names, Alan, Jeff, Sarah, and Ryan, as keys, and false otherwise.
