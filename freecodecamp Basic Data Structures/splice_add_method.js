7e42c41f410b0b13f19954bf3f77c17a49043aa4
Ijay Abby <inyangasi4@gmail.com>
2021-04-02 23:29:44 +0300

    Splice method

--- freecodecamp Basic Data Structures/splice_add_method.js
+++ freecodecamp Basic Data Structures/splice_add_method.js
@@ -4,7 +4,9 @@
 
 function htmlColorNames(arr) {
     // Only change code below this line
+
     arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+    
     // Only change code above this line
     return arr;
   }
