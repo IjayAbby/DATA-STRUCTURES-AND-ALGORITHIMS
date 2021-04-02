3677c574222add848295cf66a6b9b7bd73c5f49b
Ijay Abby <inyangasi4@gmail.com>
2021-04-02 23:38:05 +0300

    Modify an array

--- freecodecamp Basic Data Structures/add_to_array.js
+++ freecodecamp Basic Data Structures/add_to_array.js
@@ -6,8 +6,10 @@
 
 function mixedNumbers(arr) {
     // Only change code below this line
+
     arr.push(7, 'VIII', 9)
     arr.unshift('I', 2, 'three')
+    
     // Only change code above this line
     return arr;
   }
