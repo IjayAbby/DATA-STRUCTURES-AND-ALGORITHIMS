21c1f9baa2e0c883c79749a5f863da055f734fb9
Ijay Abby <inyangasi4@gmail.com>
2021-04-02 23:33:55 +0300

    Using spread operator

--- freecodecamp Basic Data Structures/copy_with_spread_operator.js
+++ freecodecamp Basic Data Structures/copy_with_spread_operator.js
@@ -6,7 +6,9 @@ function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
       // Only change code below this line
+
       newArr.push([...arr]);
+      
       // Only change code above this line
       num--;
     }
